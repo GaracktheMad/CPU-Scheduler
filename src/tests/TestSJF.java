dbadbc74c97907e5cce26ecde9608fc2c235c6d9
bruiz54 <44578945+bruiz54@users.noreply.github.com>
2018-11-02 16:16:44 -0400

    SJF Test

---
+++ src/tests/TestSJF.java
@@ -0,0 +1,21 @@
+package tests;
+
+import java.util.ArrayList;
+
+import model.BurstProcess;
+import model.SJF;
+
+public class TestSJF {
+	
+	public static void main(String args[]) {
+		SJF sjf = new SJF();
+		sjf.populateProcessList(10);
+		sjf.run();
+		
+		ArrayList<BurstProcess> processes = sjf.getProcesses();
+		
+		System.out.println();
+		sjf.getGantt().display();
+	}
+
+}
